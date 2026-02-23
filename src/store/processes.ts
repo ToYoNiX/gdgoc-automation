4f959204b23151fe14d4e84ee8bbd1cc837471c2
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2026-02-23 12:06:23 +0200

    removing test process hamasa |-(

--- src/store/processes.ts
+++ src/store/processes.ts
@@ -1,11 +1,3 @@
 import type { process_parameters } from "../common/types.js";
 
 export const processes = new Map<string, process_parameters>();
-
-processes.set("1", {
-	"name": "hamasa",
-	"filePath": "test.mp4",
-	"length": 10,
-	"downloaded": 4,
-	"uploaded": 0,
-})
